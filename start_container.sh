981289fffa9a438b9ecac185a4b3b49a28acef82
Mohan1607 <55801354+Mohan1607@users.noreply.github.com>
2024-09-20 21:35:28 +0530

    updated start container

--- start_container.sh
+++ start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-echo
+docker pull docker pull mohan1607/simple-python-app
 
 # Run the Docker image as a container
-echo
+docker run -d -p 5000:5000 docker pull mohan1607/simple-python-app
