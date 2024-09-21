87338a84c795e1433ea96d82f46fbcdd5e473bc9
Mohan1607 <55801354+Mohan1607@users.noreply.github.com>
2024-09-21 17:56:20 +0530

    updated image name

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull mohan1607/simple-python-app
+docker pull mohan1607/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 mohan1607/simple-python-app
+docker run -d -p 5000:5000 mohan1607/simple-python-flask-app
